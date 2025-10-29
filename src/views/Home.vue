13851664b52a89f6aef413eae9117daf5dce2ec3
bian <534893106@qq.com>
2025-10-29 10:24:17 +0800

    refactor(Home.vue): 修改按钮文本为更简洁的数字表示
    将"不带参设置为边龙龙"改为"不带参设置为1"，"带参设置为赵得住"改为"带参设置为2"，使界面更简洁

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,9 @@
 
     <van-button type="danger" class="button" @click="run">点我接受子组件数据</van-button>
     <div>{{ age }}</div>
-    <van-button type="warning" @click="setNameBian">不带参设置为边龙龙</van-button>
+    <van-button type="warning" @click="setNameBian">不带参设置为1</van-button>
     <div>{{ username }}</div>
-    <van-button type="warning" @click="setNameZhao">带参设置为赵得住</van-button>
+    <van-button type="warning" @click="setNameZhao">带参设置为2</van-button>
 
     <van-button type="warning" @click="showT">遮罩层</van-button>
     <van-overlay :show="show" @click="show = false">
